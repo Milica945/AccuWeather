e53d7b7f5333b17a530d1811cb10eee6545585c1
Milica Milosevic <milicamilossevicc@gmail.com>
2025-07-18 23:13:08 +0200

    New testes added.
    Extended test coverage

--- accu_weather_testes.py
+++ accu_weather_testes.py
@@ -108,6 +108,112 @@ def test_data_suite_link_navigation():
     print('The redirection to data suite works.')
     close_browser(driver)
 
+def test_data_suite_product_services_redirect():
+    driver=open_site('https://www.accuweather.com/')
+    print('Site is open.')
+    driver.find_element(By.XPATH, "//*[@id='afb-nav-container']/div/ul/li[4]/a").click()
+    sleep(2)
+    print('Clicked on the "Data Suite" link.')
+    original_tab = driver.current_window_handle
+    for tab in driver.window_handles:
+        if tab != original_tab:
+            driver.switch_to.window(tab)
+            break
+    print('Switched to the "Data Suite" tab.')
+    driver.find_element(By.XPATH,"//*[@id='hs_cos_wrapper_navigation-primary']/ul/li[1]/a").click()
+    sleep(2)
+    print('Clicked on the "Products & Services" link.')
+    assert driver.find_element(By.XPATH,"//*[@id='hs_cos_wrapper_module_166481382230911']/ul/li[2]/span").text=='PRODUCTS & SERVICES','Fail'
+    print('Redirection to "Products & Services" page successful.')
+    close_browser(driver)
+
+def test_dropdown_option_change_to_videos():
+    driver=open_site('https://www.accuweather.com/')
+    print('Site is open.')
+    driver.find_element(By.XPATH,"/html/body/div/div[1]/div[3]/div/div[1]/div[1]/div/div/div[1]").click()
+    sleep(1)
+    print('Dropdown menu opened.')
+    driver.find_element(By.XPATH,"/html/body/div/div[1]/div[3]/div/div[1]/div[1]/div/div/div[2]/a[3]/span").click()
+    sleep(2)
+    print('"Videos" option selected from dropdown.')
+    assert driver.find_element(By.XPATH,"/html/body/div/div[1]/div[3]/div/div[1]/div[1]/div/div/div[1]/span").text=='Videos', 'Fail'
+    print('Dropdown successfully switched to "Videos" and is functioning as expected.')
+    close_browser(driver)
+
+def test_forensics_button_redirects_to_correct_url():
+    driver=open_site('https://www.accuweather.com/')
+    print('Site is open.')
+    driver.find_element(By.XPATH,"//*[@id='afb-nav-container']/div/ul/li[5]/a").click()
+    sleep(2)
+    print('Clicked on "Forensics" button.')
+    second_tab = driver.current_window_handle
+    tabs = driver.window_handles
+    for tab in tabs:
+        if tab != second_tab:
+            driver.switch_to.window(tab)
+            break
+    sleep(1)
+    assert driver.current_url== 'https://afb.accuweather.com/accuweather-forensicservices','Fail'
+    print('"Forensics" page opened successfully.')
+    close_browser(driver)
+
+def test_contact_us_functionality():
+    driver=open_site('https://www.accuweather.com/')
+    print('Site is open.')
+    element= driver.find_element(By.XPATH,"/html/body/div/div[10]/span/div/div[2]/div[1]/div[1]/div[2]/a[6]")
+    print('Found "Contact Us" link.')
+    driver.execute_script("arguments[0].scrollIntoView({block : 'center'});",element)
+    sleep(2)
+    print('Scrolled to "Contact Us" link.')
+    driver.find_element(By.XPATH,"/html/body/div/div[10]/span/div/div[2]/div[1]/div[1]/div[2]/a[6]").click()
+    sleep(2)
+    second_tab= driver.current_window_handle
+    tabs = driver.window_handles
+    for tab in tabs:
+        if tab != second_tab:
+            driver.switch_to.window(tab)
+            break
+    print('Switched to "Contact Us" tab.')
+    sleep(1)
+    assert driver.current_url=='https://www.accuweather.com/en/contact', 'Fail'
+    print('Redirection to "Contact Us" page works correctly.')
+    close_browser(driver)
+
+def test_accuweather_api_reference_access():
+    driver=open_site('https://www.accuweather.com/')
+    print('Site is open.')
+    element=driver.find_element(By.XPATH,"/html/body/div/div[10]/span/div/div[2]/div[2]/div/div[2]/a[4]")
+    driver.execute_script("arguments[0].scrollIntoView({block : 'center'});",element)
+    sleep(2)
+    print('Scrolled on "API Reference" link.')
+    driver.find_element(By.XPATH,"/html/body/div/div[10]/span/div/div[2]/div[2]/div/div[2]/a[4]").click()
+    sleep(2)
+    print('Clicked on "API Reference link."')
+    original_tab=driver.current_window_handle
+    for tab in driver.window_handles:
+        if tab != original_tab:
+            driver.switch_to.window(tab)
+            break
+    print('Switched to "API Reference" tab.')
+    driver.find_element(By.XPATH,"//*[@id='navbar']/div/div/div/nav/ul[1]/li[1]/a").click()
+    sleep(2)
+    print('Clicked on first navigation link inside "API Reference" page.')
+    assert driver.find_element(By.XPATH,"/html/body/div[4]/section/div/div/div/h1").text== 'API Reference','Fail'
+    print('Redirection to API reference works correctly.')
+    close_browser(driver)
+
+def test_air_quality_link_functionality():
+    driver=open_site('https://www.accuweather.com/')
+    print('Site is open.')
+    driver.find_element(By.CSS_SELECTOR,"a[data-location-key='298198']").click()
+    sleep(2)
+    print('Clicked on location link.')
+    driver.find_element(By.XPATH,"/html/body/div/div[3]/div/div[3]/a[7]/span").click()
+    sleep(2)
+    print('Clicked on "Air Quality" button.')
+    assert driver.find_element(By.XPATH,"//*[@id='current']/div/h2").text=='CURRENT AIR QUALITY','Fail'
+    print('"Air Quality" page is displayed correctly.')
+    close_browser(driver)
 
 def close_browser(driver):
     driver.close()
